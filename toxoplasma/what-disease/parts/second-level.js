20c0c7a4ededc8a178337197ba995c1da0254092
GiuseppeIZS <g.cerratti@izs.it>
2024-07-17 00:12:15 +0200

    prima versione

---
+++ toxoplasma/what-disease/parts/second-level.js
@@ -0,0 +1,24 @@
+export function getSecondLevelTemplate(data, type, descr){ return `
+    <div class="modal-close sl-close">
+        <img width="24" src="images/icons/ic_arrow_left.svg">
+        <div>
+            <span>Back</span>
+            <small>${descr}</small>
+        </div>
+    </div>
+    <div class="sl-box slb1">
+        <h1>What<br>disease</h1>
+    </div>
+    ${getSlCollectionTemplate(data.secondLevelItems, type, descr)}
+`}
+
+function getSlCollectionTemplate(secondLevelItems, type, descr){
+    let t = '';
+    secondLevelItems.forEach((items, i) => {
+        t += `<div id="slb${i+2}" class="sl-box slb${i+2} ${type}" data-description="${descr}">
+                <img src="${items.img}">
+                <h2>${items.title}</h2>
+            </div>`
+    });
+    return t;
+}
---
+++ toxoplasma/what-disease/parts/whoisatrisk.js
@@ -0,0 +1,51 @@
+export function getWhoIsAtRiskTemplate(data, type, descr){ 
+  let t = `
+  <div class="modal-close d-close">
+    <img width="24" src="images/icons/ic_arrow_left.svg">
+    <div>
+        <span>Back</span>
+        <small>${descr}</small>
+    </div>
+  </div>
+    <div class="detail-modal-body d-slb2">
+      <img src="${data.backgroundImage.src}" class="l-img">
+      <div class="d-modal-contents">
+        <h2>Who is at risk?</h2>
+        ${getText(data)}
+        ${getCollectionTemplate(data, type)}
+      </div>
+    </div>`
+  return t;
+  }
+
+
+function getText(data){
+    let t = '';
+    if (data.text!==''){
+        t += `
+        <div class="incubation-container">
+          <div>
+            <span>${data.text}</span>
+          </div>
+        </div>
+        `
+    }
+    return t;
+}
+
+function getCollectionTemplate(data, type){
+    let t = '';
+    if (data.collection.length>0) {
+        t += `<div class="${type} risk list-container">`;
+        data.collection.forEach((elem) => {
+        t += `<div class="w-elem">
+                <img src="${elem.img}" style="${elem.customStyle}">
+                <strong>${elem.name}</strong>
+                <small>${elem.abstract}</small>
+              </div>`
+        });
+        t += `</div>`
+    }
+    return t;
+}
+
