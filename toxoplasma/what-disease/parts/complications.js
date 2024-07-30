5040269ad6db39762bf87acf8a17dc5aba22afce
GiuseppeIZS <g.cerratti@izs.it>
2024-07-31 01:03:43 +0200

    review toxo

--- toxoplasma/what-disease/parts/complications.js
+++ toxoplasma/what-disease/parts/complications.js
@@ -10,11 +10,15 @@ export function getComplicationsTemplate(data, type, descr){ return `
       <img src="${data.backgroundImage.src}" class="l-img">
       <div class="d-modal-contents">
         <h2>Complications</h2>
-        ${getCollectionTemplate(data, type)}
+        ${getAnimalOrHumanTemplate(data, type)}
         </div>
       </div>
     </div>
 `}
+
+function getAnimalOrHumanTemplate(data, type){
+  return (type === "humans" ? getHumansCollectionTemplate(data, type) : getCollectionTemplate(data, type))
+}
 
 function getText(data){
   let t = '';
@@ -39,6 +43,41 @@ if (data!=undefined && data!==''){
 }
 return t;
 }
+
+function getHumansCollectionTemplate(data, type){
+  let body = `
+  <div>
+    <button id="h-immuno" class="phases-btn active" data-phases="immuno">Immunocompromised individuals</button>
+    <button id="h-first" class="phases-btn" data-phases="first">First infection during pregnancy</button>
+  </div>
+  <div id="immuno" class="phases-panel">
+    ${getHumansCollection(data.phases.immuno, type)}
+  </div>
+
+  <div id="first" class="phases-panel d-none">
+    ${getHumansCollection(data.phases.first, type)}
+  </div>
+  `
+  return body;
+}
+
+function getHumansCollection(obj, type){
+  let t = '';
+  t += `<div class="${type} symptoms list-container">`;
+  t += getPhasesDescr(obj.phaseDescr);
+  t += `<div class="ph-collection-container">`
+  obj.collection.forEach((item) => {
+    t += `<div>
+          <img src="${item.img}">
+          <strong>${item.name}</strong>
+          <small>${item.abstract}</small>
+        </div>`
+  });
+  t += `</div>`
+  t += `</div>`
+  return t;
+}
+
 
 function getCollectionTemplate(data, type){
   let t = '';
--- toxoplasma/what-disease/parts/symptoms.js
+++ toxoplasma/what-disease/parts/symptoms.js
@@ -11,10 +11,14 @@ export function getSymptomTemplate(data, type, descr){ return `
       <div class="d-modal-contents">
         <h2>Symptoms</h2>
         ${getIncubation(data)}
-        ${getSymptomCollectionTemplate(data, type)}
+        ${getAnimalOrHumanTemplate(data, type)}
       </div>
     </div>
 `}
+
+function getAnimalOrHumanTemplate(data, type){
+  return (type === "humans" ? getSymptomCollectionTemplate(data, type) : getAnimalSymptomTemplate(data, type))
+}
 
 function getIncubation(data){
     let t = '';
@@ -76,3 +80,57 @@ function getSymptomCollectionTemplate(data, type){
     }
     return t;
 }
+
+function getAnimalSymptomTemplate(data, type){
+  let body = `
+  <div style="margin-bottom: 12px">
+    <button id="h-cat" class="phases-btn active" data-phases="cat">Cat</button>
+    <button id="h-other" class="phases-btn" data-phases="other">Other animals</button>
+  </div>
+  ${getText("Most animals infected with Toxoplasma gondii are asymptomatic.<br><br>")}
+  <div id="cat" class="phases-panel">
+    ${getAnimalCollection(data.phases.cat, type)}
+  </div>
+
+  <div id="other" class="phases-panel d-none">
+    ${getAnimalCollection(data.phases.other, type)}
+  </div>
+`
+return body;
+}
+
+function getAnimalCollection(group, type){
+  let t = '';
+  if (group.length>0) {
+    t += `<div class="${type} symptoms list-container">`;
+    group.forEach((elem) => {
+      t += `<div class="ph-collection-container">
+              <div class="phases-animal-text-container">${elem.text}</div>`
+      elem.collection.forEach((item) => {
+        t += `<div>
+              <img src="${item.img}">
+              <strong>${item.name}</strong>
+              <small>${item.abstract}</small>
+            </div>`
+      });
+      t += `</div>`
+    });
+    t += `</div>`
+  }
+  return t;
+  /*
+  let t = '';
+  t += `<div class="ph-collection-container">`
+    collection.forEach((item) => {
+      t += `<div>
+            <img src="${item.img}">
+            <strong>${item.name}</strong>
+            <small>${item.abstract}</small>
+          </div>`
+    });
+  t += `</div>`;
+  return t;
+  */
+}
+
+
